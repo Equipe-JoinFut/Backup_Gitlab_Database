19d16c4230c44cc1d800e8ae354c509e4d299330
F4NT0 <gabriel.stundner@acad.pucrs.br>
2022-09-01 19:22:32 -0300

    Script datagrip iniciador

---
+++ Script_Linux/DatagripStarter.sh
@@ -0,0 +1,15 @@
+#-----------------------------------
+# COMO RODAR O DATAGRIP PELO CONSOLE
+#-----------------------------------
+
+
+# 1) Voltar do Desktop para o diretório (~/)
+cd ..
+# 2) Acessar o Download
+cd Downloads/
+# 3) Acessando o diretório do Datagrip
+cd DataGrip-2022.2.2/
+# 4) Acesse a página bin/
+cd bin/
+# 5) Rodando o script do Datagrip
+./datagrip.sh
