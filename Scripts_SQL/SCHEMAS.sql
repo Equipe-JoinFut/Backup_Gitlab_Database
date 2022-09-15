e81b733e4bf65567db52a89f58a1fac88702356f
Gabriel Fanto Stundner <gabriel.stundner@acad.pucrs.br>
2022-09-15 12:06:12 +0000

    Update Scripts_SQL/SCHEMAS.sql

---
+++ Scripts_SQL/SCHEMAS.sql
@@ -0,0 +1,9 @@
+----------------------------------------------
+--            SCHEMAS DO PROJETO           ---
+----------------------------------------------
+-- Deve ser rodado antes de rodar o projeto --
+----------------------------------------------
+
+CREATE SCHEMA IF NOT EXISTS personas;
+CREATE SCHEMA IF NOT EXISTS informations;
+CREATE SCHEMA IF NOT EXISTS examples;
--- Scripts_SQL/TemplateSchema.sql
+++
@@ -1,7 +0,0 @@
-----------------------------------
--- SCHEMAS CRIADOS PARA O PROJETO
-----------------------------------
-
-CREATE SCHEMA IF NOT EXISTS personas;
-CREATE SCHEMA IF NOT EXISTS informations;
-CREATE SCHEMA IF NOT EXISTS examples;
