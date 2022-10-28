342e4c3133846c1b1becafa3e53944de36909759
Gabriel Fanto Stundner <gabriel.stundner@acad.pucrs.br>
2022-10-28 00:52:30 -0300

    #16 Atualizado os testes

---
+++ Scripts_SQL/DROP-SCHEMAS.sql
@@ -0,0 +1,8 @@
+----------------------
+-- DROPS DOS SCHEMAS
+----------------------
+
+drop schema examples cascade;
+drop schema informations cascade;
+drop schema personas cascade;
+drop schema selections cascade;
